4485fc7c9b032b394713500bf123e1b422f548c7
Christina Burbulevych <christina.brb@gmail.com>
2020-07-28 08:53:13 +0300

    task 3

--- task-3.js
+++ task-3.js
@@ -6,11 +6,32 @@
 //В протовном случае, если введен пароль который совпадает со значением константы ADMIN_PASSWORD, записать в message строку 'Добро пожаловать!'
 //В противном случае, то есть если ни одно из предыдущих условий не выполнилось, записать в message строку 'Доступ запрещен, неверный пароль!'
 //После всех проверок вывести в alert значение переменной message.
+const userPassword = 'jqueryismyjam';
 
 const ADMIN_PASSWORD = 'jqueryismyjam';
-let message;
 
-// 1 if
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const ACCESS_IS_ALLOWED = 'Добро пожаловать!';
+const ACCESS_DENIED = 'Доступ запрещен, неверный пароль!';
+let message ='';
 
+// Write code under this line
+if (userPassword === null) {
+    message = CANCELED_BY_USER;
+} else if (userPassword != ADMIN_PASSWORD) {
+    message = ACCESS_DENIED;
+ } else {
+    message =  ACCESS_IS_ALLOWED
+ }
+console.log(message);
+//если userPassword равно 'jqueryismyjam' 
+// то значение message будет равно
+// 'Добро пожаловать!'
 
-// proverka
+//если userPassword равно  null 
+// то значение message будет равно  
+// 'Отменено пользователем!'
+
+//если userPassword равно '123' 
+// то значение message будет равно
+// 'Доступ запрещен, неверный пароль!'
